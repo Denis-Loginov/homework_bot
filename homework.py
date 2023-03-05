3b202ac1ed9086cbc94950c369eb2d2cb46f1b70
Логинов Денис <loginov.day@yandex.ru>
2023-03-05 19:33:32 +0400

    Вер.1.2 Еще чуть разобрался с логированием и по мелочи

--- homework.py
+++ homework.py
@@ -20,7 +20,6 @@ TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-PAYLOAD = {'from_date': 1676246400}
 
 HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
@@ -29,13 +28,6 @@ HOMEWORK_VERDICTS = {
 }
 
 logger = logging.getLogger(__name__)
-
-if __name__ == '__main__':
-    logger.setLevel(logging.INFO)
-    handler = StreamHandler(sys.stdout)
-    logger.addHandler(handler)
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
 
 
 class NoSendMessageException(Exception):
@@ -54,7 +46,7 @@ def send_message(bot, message):
         logging.debug('Сообщение отправлено успешно')
     except telegram.error.TelegramError as error:
         logger.error(f'Сбой при отправке сообщения: {error}')
-        raise NoSendMessageException(f'Сбой при отправке сообщения: {error}')
+        raise NoSendMessageException(f'Сбой при отправке : {error}')
 
 
 def get_api_answer(timestamp):
@@ -79,7 +71,8 @@ def check_response(response):
     if response.get('homeworks') is None:
         raise KeyError('В ответе API нет ключей')
     elif not isinstance(response['homeworks'], list):
-        raise TypeError('Получен список вместо ожидаемого словаря')
+        raise TypeError(
+            f'в ответе {response} по ключу homeworks ожидался список')
 
 
 def parse_status(homework):
@@ -88,9 +81,8 @@ def parse_status(homework):
         homework_name = homework['homework_name']
         homework_status = homework['status']
     except KeyError as error:
-        logger.error(f'В ответе API отсутствуют ключи: {error}')
+        raise (f'В ответе API отсутствуют ключи: {error}')
     if homework_status not in HOMEWORK_VERDICTS:
-        logger.error('В ответе API отсутствует staus домашней работы')
         raise KeyError('В ответе API отсутствует staus домашней работы')
     verdict = HOMEWORK_VERDICTS[homework_status]
     return (f'Изменился статус проверки работы "{homework_name}". {verdict}')
@@ -98,11 +90,11 @@ def parse_status(homework):
 
 def main():
     """Основная логика работы бота."""
-    if check_tokens() is False:
+    if not check_tokens():
         logger.critical('Отсутствуют переменные окружения')
         sys.exit('Отсутствуют переменные окружения')
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    timestamp = PAYLOAD
+    timestamp = {'from_date': 1676246400}
     error_mesage = ''
     status = ''
     while True:
@@ -111,7 +103,7 @@ def main():
             if response:
                 logging.debug(f'отправлен запрос к API: {timestamp}')
             check_response(response)
-            if len(response['homeworks']) != 0:
+            if response['homeworks']:
                 new_status = parse_status(response['homeworks'][0])
                 if new_status != status:
                     send_message(bot, new_status)
@@ -119,16 +111,19 @@ def main():
             current_date = response['current_date']
             timestamp = {'from_date': current_date}
         except Exception as error:
-            logger.error(error)
             new_error_mesage = f'Сбой в работе программы: {error}'
+            logger.error(new_error_mesage)
             if new_error_mesage != error_mesage:
                 send_message(bot, new_error_mesage)
                 error_mesage = new_error_mesage
-            else:
-                logger.error(new_error_mesage)
         finally:
             time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
+    logger.setLevel(logging.INFO)
+    handler = StreamHandler(sys.stdout)
+    logger.addHandler(handler)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
     main()
